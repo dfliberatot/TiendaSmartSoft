0d01b0d318e0c3a5486a59aa3af43a28300c9dc6
dfliberatot <dfliberatot@ut.edu.co>
2021-09-26 23:53:55 -0500

    adiciones FrameWork
    Adiciones hechas por el framework para el correcto funcionamiento del aplicativo

---
+++ Back Spring/src/main/java/com/smartsoft/Repository/iclienteRepository.java
@@ -0,0 +1,9 @@
+package com.smartsoft.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smartsoft.Model.Cliente;
+
+public interface iclienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
